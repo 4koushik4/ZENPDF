8223bb1ed7155e063adbfc50ed0d56ab5e0aa7c0
4koushik4 <koushikjuluri4444@gmail.com>
2025-06-08 19:56:13 +0530

    chore: add react-toastify dependency and configure toast notifications

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import MergePDF from "./components/MergePDF";
@@ -45,106 +47,109 @@ const Layout = ({ children }) => {
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={
-          <Layout>
-            <Home0 />
-          </Layout>
-        } />
-        <Route path="/home" element={
-          <Layout>
-            <Home />
-          </Layout>
-        } />
-        <Route path="/merge" element={
-          <Layout>
-            <MergePDF />
-          </Layout>
-        } />
-        <Route path="/contact" element={
-          <Layout>
-            <Contact />
-          </Layout>
-        } />
-        <Route path="/adminfeedback" element={
-          <Layout>
-            <AdminFeedback />
-          </Layout>
-        } />
-        <Route path="/split" element={
-          <Layout>
-            <SplitPDF />
-          </Layout>
-        } />
-        <Route path="/compress" element={
-          <Layout>
-            <CompressPDF />
-          </Layout>
-        } />
-        <Route path="/extract-pages" element={
-          <Layout>
-            <ExtractPages />
-          </Layout>
-        } />
-        <Route path="/remove-pages" element={
-          <Layout>
-            <RemovePages />
-          </Layout>
-        } />
-        <Route path="/rotate-pdf" element={
-          <Layout>
-            <RotatePDF />
-          </Layout>
-        } />
-        <Route path="/reorder-pages" element={
-          <Layout>
-            <ReorderPages />
-          </Layout>
-        } />
-        <Route path="/image-to-pdf" element={
-          <Layout>
-            <ImageToPDF />
-          </Layout>
-        } />
-        <Route path="/watermark" element={
-          <Layout>
-            <WatermarkPDF />
-          </Layout>
-        } />
-        <Route path="/Fundraiser" element={
-          <Layout>
-            <Fundraiser />
-          </Layout>
-        } />
-        <Route path="/password-protect" element={
-          <Layout>
-            <PasswordProtect />
-          </Layout>
-        } />
-        <Route path="/sign-pdf" element={
-          <Layout>
-            <SignPDF />
-          </Layout>
-        } />
-        <Route path="/unlock-pdf" element={
-          <Layout>
-            <UnlockPDF />
-          </Layout>
-        } />
-        <Route path="/page-numbers" element={
-          <Layout>
-            <AddPageNumbers />
-          </Layout>
-        } />
-        <Route path="/faq" element={<FAQ />} />
-        <Route path="/support" element={<Support />} />
-        <Route path="/updates" element={<Updates />} />
-        <Route path="/feedback" element={<Feedback />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-        <Route path="/terms" element={<TermsOfService />} />
-        {/* Redirect all unknown routes to Home Page */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+      <div className="App">
+        <ToastContainer position="top-right" autoClose={3000} />
+        <Routes>
+          <Route path="/" element={
+            <Layout>
+              <Home0 />
+            </Layout>
+          } />
+          <Route path="/home" element={
+            <Layout>
+              <Home />
+            </Layout>
+          } />
+          <Route path="/merge" element={
+            <Layout>
+              <MergePDF />
+            </Layout>
+          } />
+          <Route path="/contact" element={
+            <Layout>
+              <Contact />
+            </Layout>
+          } />
+          <Route path="/adminfeedback" element={
+            <Layout>
+              <AdminFeedback />
+            </Layout>
+          } />
+          <Route path="/split" element={
+            <Layout>
+              <SplitPDF />
+            </Layout>
+          } />
+          <Route path="/compress" element={
+            <Layout>
+              <CompressPDF />
+            </Layout>
+          } />
+          <Route path="/extract-pages" element={
+            <Layout>
+              <ExtractPages />
+            </Layout>
+          } />
+          <Route path="/remove-pages" element={
+            <Layout>
+              <RemovePages />
+            </Layout>
+          } />
+          <Route path="/rotate-pdf" element={
+            <Layout>
+              <RotatePDF />
+            </Layout>
+          } />
+          <Route path="/reorder-pages" element={
+            <Layout>
+              <ReorderPages />
+            </Layout>
+          } />
+          <Route path="/image-to-pdf" element={
+            <Layout>
+              <ImageToPDF />
+            </Layout>
+          } />
+          <Route path="/watermark" element={
+            <Layout>
+              <WatermarkPDF />
+            </Layout>
+          } />
+          <Route path="/Fundraiser" element={
+            <Layout>
+              <Fundraiser />
+            </Layout>
+          } />
+          <Route path="/password-protect" element={
+            <Layout>
+              <PasswordProtect />
+            </Layout>
+          } />
+          <Route path="/sign-pdf" element={
+            <Layout>
+              <SignPDF />
+            </Layout>
+          } />
+          <Route path="/unlock-pdf" element={
+            <Layout>
+              <UnlockPDF />
+            </Layout>
+          } />
+          <Route path="/page-numbers" element={
+            <Layout>
+              <AddPageNumbers />
+            </Layout>
+          } />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/support" element={<Support />} />
+          <Route path="/updates" element={<Updates />} />
+          <Route path="/feedback" element={<Feedback />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/terms" element={<TermsOfService />} />
+          {/* Redirect all unknown routes to Home Page */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </div>
     </Router>
   );
 };
